48ca40af9478dbb9916d6bfccaefe6531ba32af1
Satyam Kushwaha <sk7555911@gmail.com>
2026-06-21 00:32:14 +0530

    Initial commit

--- Sorting/BUBBLE_SORT.cpp
+++ Sorting/BUBBLE_SORT.cpp
@@ -1,17 +1,6 @@
 #include<iostream>
 using namespace std;
 
-void selection_sort(int arr[],int n){
-    int i,j,min;
-    for(i=0;i<n-1;i++){
-        min=i;
-        for(j=i+1;j<n;j++){
-            if(arr[j]<arr[min])
-            min=j;
-        }
-        swap(arr[i],arr[min]);
-    }
-}
 void bubble_sort(int arr[],int n){
     int i,j;
     for(i=0;i<n-1;i++){
---
+++ Sorting/SELECTION_SORT.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+using namespace std;
+
+void selection_sort(int arr[],int n){
+    int i,j,min;
+    for(i=0;i<n-1;i++){
+        min=i;
+        for(j=i+1;j<n;j++){
+            if(arr[j]<arr[min])
+            min=j;
+        }
+        swap(arr[i],arr[min]);
+    }
+}
+
+void swap(int &a,int &b){
+    int temp;
+    temp=a;
+    a=b;
+    b=temp;
+
+
+}
+int main() {
+    int i,n;
+    
+    cin>>n;
+    int arr[n];
+    for(i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    selection_sort(arr,n);
+    for(i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+
+    }
+return 0;
+
+    
+   
+}
